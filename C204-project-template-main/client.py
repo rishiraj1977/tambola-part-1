3fa2bf61ae81c4b5143bf2a9f3fcbf5e7bcbf92d
RISHIRAJ <58257428+rishiraj1977@users.noreply.github.com>
2023-03-03 10:33:40 +0530

    updated

--- C204-project-template-main/client.py
+++ C204-project-template-main/client.py
@@ -1,22 +1,118 @@
-
 import socket
 from tkinter import *
+import tkinter as tk
 from  threading import Thread
+import random
 from PIL import ImageTk, Image
+import platform
+
 
 screen_width = None
 screen_height = None
 
 SERVER = None
-PORT = None
-IP_ADDRESS = None
-
+PORT  = 8000
+IP_ADDRESS = '127.0.0.1'
+playerName = None
 
 canvas1 = None
+canvas2 = None
 
-playerName = None
 nameEntry = None
 nameWindow = None
+gameWindow = None
+
+ticketGrid  = []
+currentNumberList = []
+flashNumberList = []
+flashNumberLabel = None
+
+
+def createTicket():
+    global gameWindow
+    global ticketGrid
+    # Ticket Frame
+    mianLable = Label(gameWindow, width=65, height=16,relief='ridge', borderwidth=5, bg='white')
+    mianLable.place(x=95, y=119)
+
+    xPos = 105
+    yPos = 130
+    for row in range(0, 3):
+        rowList = []
+        for col in range(0, 9):
+            if(platform.system() == 'Darwin'):
+                # For Mac users
+                boxButton = Button(gameWindow,
+                font = ("Chalkboard SE",18),
+                borderwidth=3,
+                pady=23,
+                padx=-22,
+                bg="#fff176",
+                highlightbackground='#fff176',
+                activebackground='#c5e1a5')
+
+
+                boxButton.place(x=xPos, y=yPos)
+            else:
+                # For windows users
+                boxButton = tk.Button(gameWindow, font=("Chalkboard SE",30), width=3, height=2,borderwidth=5, bg="#fff176")
+                boxButton.place(x=xPos, y=yPos)
+
+            rowList.append(boxButton)
+            xPos += 64
+       
+        ticketGrid.append(rowList)
+        xPos = 105
+        yPos +=82
+
+def placeNumbers():
+    global ticketGrid
+    global currentNumberList
+
+    for row in range(0,3):
+        randomColList = []
+        counter = 0
+        while counter<=4:
+            randomCol = random.randint(0,8)
+            if(randomCol not in randomColList):
+                randomColList.append(randomCol)
+                counter+=1
+
+def gameWindow():
+    global gameWindow
+    global canvas2
+    global screen_width
+    global screen_height
+    global dice
+    global winingMessage
+    global resetButton
+    global flashNumberLabel
+
+    gameWindow = Tk()
+    gameWindow.title("Tambola Family Fun")
+    gameWindow.geometry('800x600')
+
+    screen_width = gameWindow.winfo_screenwidth()
+    screen_height = gameWindow.winfo_screenheight()
+
+    bg = ImageTk.PhotoImage(file = "./assets/background.png")
+
+    canvas2 = Canvas( gameWindow, width = 500,height = 500)
+    canvas2.pack(fill = "both", expand = True)
+
+    # Display image
+    canvas2.create_image( 0, 0, image = bg, anchor = "nw")
+
+    # Add Text
+    canvas2.create_text( screen_width/4.5,50, text = "Tambola Family Fun", font=("Chalkboard SE",50), fill="#3e2723")
+
+    createTicket()
+    placeNumbers()
+
+    flashNumberLabel = canvas2.create_text(400,screen_height/2.3, text = "Waiting for other players to join...", font=("Chalkboard SE",30), fill="#3e2723")
+
+    gameWindow.resizable(True, True)
+    gameWindow.mainloop()
 
 def saveName():
     global SERVER
@@ -25,22 +121,22 @@ def saveName():
     global nameEntry
 
     playerName = nameEntry.get()
-    nameEntry.delete(0,END)
+    nameEntry.delete(0, END)
     nameWindow.destroy()
 
     SERVER.send(playerName.encode())
+
+    gameWindow()
 
 def askPlayerName():
     global playerName
     global nameEntry
     global nameWindow
     global canvas1
-    global screen_width
-    global screen_height
 
     nameWindow  = Tk()
-    nameWindow.title("Tambola")
-    nameWindow.attributes('-fullscreen',True)
+    nameWindow.title("Tambola Family Fun")
+    nameWindow.geometry('800x600')
 
 
     screen_width = nameWindow.winfo_screenwidth()
@@ -50,15 +146,15 @@ def askPlayerName():
 
     canvas1 = Canvas( nameWindow, width = 500,height = 500)
     canvas1.pack(fill = "both", expand = True)
-    
+    # Display image
     canvas1.create_image( 0, 0, image = bg, anchor = "nw")
-    canvas1.create_text( screen_width/2, screen_height/5, text = "Enter Name", font=("Chalkboard SE",100), fill="white")
+    canvas1.create_text( screen_width/4.5,screen_height/8, text = "Enter Name", font=("Chalkboard SE",60), fill="#3e2723")
 
-    nameEntry = Entry(nameWindow, width=15, justify='center', font=('Chalkboard SE', 50), bd=5, bg='white')
-    nameEntry.place(x = screen_width/2 - 220, y=screen_height/4 + 100)
+    nameEntry = Entry(nameWindow, width=15, justify='center', font=('Chalkboard SE', 30), bd=5, bg='white')
+    nameEntry.place(x = screen_width/7, y=screen_height/5.5 )
 
-    button = Button(nameWindow, text="Save", font=("Chalkboard SE", 30),width=15, command=saveName, height=2, bg="#80deea", bd=3)
-    button.place(x = screen_width/2 - 130, y=screen_height/2 - 30)
+    button = tk.Button(nameWindow, text="Save", font=("Chalkboard SE", 30),width=11, command=saveName, height=2, bg="red", bd=3)
+    button.place(x = screen_width/6, y=screen_height/4)
 
     nameWindow.resizable(True, True)
     nameWindow.mainloop()
@@ -68,12 +164,9 @@ def setup():
     global PORT
     global IP_ADDRESS
 
-    PORT  = 5000
-    IP_ADDRESS = '127.0.0.1'
 
     SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER.connect((IP_ADDRESS, PORT))
 
     askPlayerName()
-
 setup()
